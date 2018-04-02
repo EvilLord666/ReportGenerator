01725be893ce7cbe3262f36e5c721a21ccc83756
Michael Ushakov <um.nix.user@gmail.com>
2018-04-02 17:29:47 +0300

    intermediate: defining interface

---
+++ ReportGenerator/ReportGeneratorCore/Data/Parameters/StoredProcedureParameter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace ReportGenerator.Core.Data.Parameters
+{
+    public class StoredProcedureParameter
+    {
+        public StoredProcedureParameter(SqlDbType parameterType, string parameterName, object parameterValue)
+        {
+            ParameterType = parameterType;
+            ParameterName = parameterName;
+            ParameterValue = parameterValue;
+        }
+
+        public SqlDbType ParameterType { get; private set; }
+        public string ParameterName { get; private set; }
+        public object ParameterValue { get; private set; }
+    }
+}
--- ReportGenerator/ReportGeneratorCore/Extractor/IDbExtractor.cs
+++ ReportGenerator/ReportGeneratorCore/Extractor/IDbExtractor.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using ReportGenerator.Core.Data;
+using ReportGenerator.Core.Data.Parameters;
 
 namespace ReportGenerator.Core.Extractor
 {
     public interface IDbExtractor
     {
-        DbData Extract(string storedPocedureName);
+        DbData Extract(string storedPocedureName, IList<StoredProcedureParameter> parameters);
+
+        DbData Extract(string viewName, IList<ViewParameter> whereParameters, IList<ViewParameter> orderByParameters,
+                       IList<ViewParameter> groupByParameters);
     }
 }
