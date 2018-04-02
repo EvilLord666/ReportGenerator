b73ddab5c54c4735850042202095164e032f46a9
Michael Ushakov <um.nix.user@gmail.com>
2018-04-02 22:02:59 +0300

    Implementation of IDbExtracor (empty)

---
+++ ReportGenerator/ReportGeneratorCore/Extractor/SimpleDbExtractor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReportGenerator.Core.Data;
+using ReportGenerator.Core.Data.Parameters;
+
+namespace ReportGenerator.Core.Extractor
+{
+    public class SimpleDbExtractor : IDbExtractor
+    {
+        public DbData Extract(string storedPocedureName, IList<StoredProcedureParameter> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DbData Extract(string viewName, ViewParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
