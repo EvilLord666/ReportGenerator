3b197f4f8bc84dd10d9ddcaa16e63c4388beb22a
Michael Ushakov <um.nix.user@gmail.com>
2018-04-06 00:19:30 +0300

    partial cleanup

--- ReportGenerator/ReportGenerator.Core.Tests/Extractor/TestSimpleDbExtractor.cs
+++ ReportGenerator/ReportGenerator.Core.Tests/Extractor/TestSimpleDbExtractor.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.IO;
 using System.Text;
 using ReportGenerator.Core.StatementsGenerator;
+using ReportGenerator.Core.Tests.TestUtils;
 using Xunit;
 
 namespace ReportGenerator.Core.Tests.Extractor
@@ -28,14 +29,16 @@ namespace ReportGenerator.Core.Tests.Extractor
 
         private void SetUpTestData()
         {
-            string masterDbConnString = GetConnectionString(MasterDatabase);
+            /*string masterDbConnString = GetConnectionString(MasterDatabase);
             using (SqlConnection connection = new SqlConnection(masterDbConnString))
             {
                 connection.Open();
                 SqlCommand createCommand = new SqlCommand(string.Format(CreateDatabaseStatementTemplate, TestDatabase), connection);
                 createCommand.ExecuteNonQuery();
                 connection.Close();
-            }
+            }*/
+
+            TestDatabaseManager.CreateDatabase(Server, TestDatabase, true);
 
             string testDbConnString = GetConnectionString(TestDatabase);
             using (SqlConnection connection = new SqlConnection(testDbConnString))
@@ -54,14 +57,15 @@ namespace ReportGenerator.Core.Tests.Extractor
 
         private void TearDownTestData()
         {
-            string masterDbConnString = GetConnectionString(MasterDatabase);
+            TestDatabaseManager.DropDatabase(Server, TestDatabase);
+            /*string masterDbConnString = GetConnectionString(MasterDatabase);
             using (SqlConnection connection = new SqlConnection(masterDbConnString))
             {
                 connection.Open();
                 SqlCommand createCommand = new SqlCommand(string.Format(DropDatabaseStatementTemplate, TestDatabase), connection);
                 createCommand.ExecuteNonQuery();
                 connection.Close();
-            }
+            }*/
         }
 
         private string GetConnectionString(string database)
@@ -78,7 +82,7 @@ namespace ReportGenerator.Core.Tests.Extractor
         private const string MasterDatabase = "master";
 
         private string CreateDatabaseStatementTemplate = "CREATE DATABASE {0};";
-        private string DropDatabaseStatementTemplate = "ALTER DATABASE {0} SET SINGLE_USER WITH ROLLBACK IMMEDIATE;  DROP DATABASE [{0}];";
+        //private string DropDatabaseStatementTemplate = "ALTER DATABASE {0} SET SINGLE_USER WITH ROLLBACK IMMEDIATE;  DROP DATABASE [{0}];";
 
         private const string CreateDatabaseScript = @"..\..\..\DbScripts\CreateDb.sql";
         private const string InsertDataScript = @"..\..\..\DbScripts\CreateData.sql";
