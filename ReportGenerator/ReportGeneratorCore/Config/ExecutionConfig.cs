bfcc17ffce53e22a47a98ab268fc57229ee22136
Michael Ushakov <um.nix.user@gmail.com>
2018-04-08 19:09:22 +0300

    Execution config plus config writer

---
+++ ReportGenerator/ReportGeneratorCore/Config/ExecutionConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+using ReportGenerator.Core.Data.Parameters;
+
+namespace ReportGenerator.Core.Config
+{
+    public enum ReportDataSource
+    {
+        View,
+        StoredProcedure
+    }
+
+    [XmlRoot("ExecutionConfig")]
+    public class ExecutionConfig
+    {
+        public ExecutionConfig()
+        {
+        }
+
+        public ExecutionConfig(ReportDataSource dataSource, IList<StoredProcedureParameter> storedProcedureParameters,
+                               ViewParameters viewParameters)
+        {
+            DataSource = dataSource;
+            StoredProcedureParameters = storedProcedureParameters;
+            ViewParameters = viewParameters;
+        }
+
+
+        [XmlElement("DataSource")]
+        public ReportDataSource DataSource { get; set; }
+
+        [XmlElement("StoredProcedureParameters")]
+        public IList<StoredProcedureParameter> StoredProcedureParameters { get; set; }
+
+        [XmlElement("ViewParameters")]
+        public ViewParameters ViewParameters { get; set; }
+    }
+}
---
+++ ReportGenerator/ReportGeneratorCore/Config/ExecutionConfigManager.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ReportGenerator.Core.Config
+{
+    public class ExecutionConfigManager
+    {
+        public static ExecutionConfig Read(string file)
+        {
+            if (!File.Exists(file))
+                return null;
+            ExecutionConfig result;
+            using (Stream reader = new FileStream(file, FileMode.Open))
+            {
+                result = _serializer.Deserialize(reader) as ExecutionConfig;
+            }
+            return result;
+        }
+
+        public static bool Write(string file, ExecutionConfig config)
+        {
+            if (config == null)
+                return false;
+            StringWriter stringWriter = new StringWriter();
+            using (var writer = XmlWriter.Create(stringWriter))
+            {
+                _serializer.Serialize(writer, config);
+            }
+            return true;
+        }
+
+        private static XmlSerializer _serializer = new XmlSerializer(typeof(ExecutionConfig));
+    }
+}
