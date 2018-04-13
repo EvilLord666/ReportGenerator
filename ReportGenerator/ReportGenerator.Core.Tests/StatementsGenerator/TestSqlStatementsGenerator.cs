7a2979a1f116f3b5c7c11d3a1d252e8ab76c9cc3
Michael Ushakov <um.nix.user@gmail.com>
2018-04-13 21:21:28 +0300

    fixed sql generator

--- ReportGenerator/ReportGenerator.Core.Tests/StatementsGenerator/TestSqlStatementsGenerator.cs
+++ ReportGenerator/ReportGenerator.Core.Tests/StatementsGenerator/TestSqlStatementsGenerator.cs
@@ -8,9 +8,9 @@ namespace ReportGenerator.Core.Tests.StatementsGenerator
     public class TestSqlStatementsGenerator
     {
         [Theory]
-        [InlineData(true, false, false, "Citizen", "SELECT * FROM Citizen  WHERE City IN (Yekaterinburg, Moscow, Kazan) OR Age > 18 AND  District BETWEEN Northern AND Southerly")]
-        [InlineData(true, true, false, "Citizen", "SELECT * FROM Citizen  WHERE City IN (Yekaterinburg, Moscow, Kazan) OR Age > 18 AND  District BETWEEN Northern AND Southerly ORDER BY City ASC, Name DESC")]
-        [InlineData(true, true, true, "Citizen", "SELECT * FROM Citizen  WHERE City IN (Yekaterinburg, Moscow, Kazan) OR Age > 18 AND  District BETWEEN Northern AND Southerly GROUP BY Age, Name ORDER BY City ASC, Name DESC")]
+        [InlineData(true, false, false, "Citizen", "SELECT * FROM Citizen  WHERE City IN (Yekaterinburg, Moscow, Kazan) OR Age > 18 AND District BETWEEN Northern AND Southerly")]
+        [InlineData(true, true, false, "Citizen", "SELECT * FROM Citizen  WHERE City IN (Yekaterinburg, Moscow, Kazan) OR Age > 18 AND District BETWEEN Northern AND Southerly ORDER BY City ASC, Name DESC")]
+        [InlineData(true, true, true, "Citizen", "SELECT * FROM Citizen  WHERE City IN (Yekaterinburg, Moscow, Kazan) OR Age > 18 AND District BETWEEN Northern AND Southerly GROUP BY Age, Name ORDER BY City ASC, Name DESC")]
         public void TestCreateSelectStatementSuccessfully(bool wherePresent, bool orderByPresent, bool groupByPresent, string tableName, string expectedSelectStatement)
         {
             ViewParameters viewParams = GetTestParameters(wherePresent, orderByPresent, groupByPresent);
@@ -38,9 +38,9 @@ namespace ReportGenerator.Core.Tests.StatementsGenerator
             if (useWhereParameters)
             {
                 List<DbQueryParameter> whereParams = new List<DbQueryParameter>();
-                whereParams.Add(new DbQueryParameter(new[] { JoinCondition.In }, "City", string.Empty, "Yekaterinburg, Moscow, Kazan"));
+                whereParams.Add(new DbQueryParameter(null, "City", "IN", "(Yekaterinburg, Moscow, Kazan)"));
                 whereParams.Add(new DbQueryParameter(new[] { JoinCondition.Or }, "Age", ">", "18"));
-                whereParams.Add(new DbQueryParameter(new[] { JoinCondition.And, JoinCondition.Between }, "District", null, "Northern AND Southerly"));
+                whereParams.Add(new DbQueryParameter(new[] { JoinCondition.And }, "District", "BETWEEN", "Northern AND Southerly"));
                 parameters.WhereParameters = whereParams;
             }
 
