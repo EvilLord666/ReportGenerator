6b03038d430a9a7df959ed32b27af1ef62970c88
Ushakov Michael <um.nix.user@gmail.com>
2018-11-26 11:01:43 +0300

    forgot to save

--- ReportGenerator/ReportGenerator.Core.Tests/TestUtils/TestSqliteDatabaseManager.cs
+++ ReportGenerator/ReportGenerator.Core.Tests/TestUtils/TestSqliteDatabaseManager.cs
@@ -1,7 +1,42 @@
+using System;
+using System.Data.SQLite;
+using System.IO;
+
 namespace ReportGenerator.Core.Tests.TestUtils
 {
-    public class TestSqliteDatabaseManager
+    public static class TestSqLiteDatabaseManager
     {
+        public static void CreateDatabase(string database, bool drop = false)
+        {
+            SQLiteConnection.CreateFile(database);
+        }
+
+        public static void DropDatabase(string database)
+        {
+            try
+            {
+                SQLiteConnection.ClearAllPools();
+            }
+            catch (Exception e)
+            {
+            }
+
+            if (File.Exists(database))
+                File.Delete(database);
+        }
+
+        public static void ExecuteSql(string database, string sql)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(string.Format(SqLiteConnectionStringTemplate, 
+                                                                                    database, 3)))
+            {
+                connection.Open();
+                SQLiteCommand command = new SQLiteCommand(sql, connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
         
+        private const string SqLiteConnectionStringTemplate = "Data Source={0};Version={1}";
     }
 }
