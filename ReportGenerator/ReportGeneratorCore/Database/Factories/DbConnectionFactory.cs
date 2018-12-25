c1319b53acbe26e75b082adbab79e5399703a668
Ushakov Michael <um.nix.user@gmail.com>
2018-12-25 10:49:51 +0300

    developing SqlServerDbManager

--- ReportGenerator/ReportGeneratorCore/Database/Factories/DbConnectionFactory.cs
+++ ReportGenerator/ReportGeneratorCore/Database/Factories/DbConnectionFactory.cs
@@ -2,6 +2,7 @@ using System;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.Data.SQLite;
+using MySql.Data.MySqlClient;
 
 namespace ReportGenerator.Core.Database.Factories
 {
@@ -13,6 +14,8 @@ namespace ReportGenerator.Core.Database.Factories
                 return new SqlConnection(connectionString);
             if (dbEngine == DbEngine.SqLite)
                 return new SQLiteConnection(connectionString);
+            if (dbEngine == DbEngine.MySql)
+                return new MySqlConnection(connectionString);
             throw new NotImplementedException("Other db engine were not implemented yet");
         }
     }
