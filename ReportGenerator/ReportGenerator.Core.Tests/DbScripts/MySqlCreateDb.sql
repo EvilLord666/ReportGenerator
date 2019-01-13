acb1fd60e500689aa592900a39e6ea26b3f79cd2
Ushakov Michael <um.nix.user@gmail.com>
2019-01-13 20:21:14 +0300

    finally MySql works with stored procedures also

--- ReportGenerator/ReportGenerator.Core.Tests/DbScripts/MySqlCreateDb.sql
+++ ReportGenerator/ReportGenerator.Core.Tests/DbScripts/MySqlCreateDb.sql
@@ -28,9 +28,22 @@ SELECT Cz.FirstName, Cz.LastName, Cz.Age, Cz.Sex, Ci.Name AS City, R.Name AS Reg
 FROM Citizen AS Cz INNER JOIN City AS Ci ON Cz.CityId = Ci.Id
 INNER JOIN Region AS R ON Ci.RegionId = R.Id;
 
-CREATE PROCEDURE GetCitizensByCityAndAge(IN CityName NVARCHAR(200), IN PersonAge INTEGER)
+CREATE PROCEDURE GetCitizensWithCitiesByCityAndAge(IN CityName NVARCHAR(200), IN PersonAge INTEGER)
 BEGIN
     SELECT Cz.FirstName, Cz.LastName, Cz.Age, Ci.Name AS CityName FROM Citizen AS Cz
 	INNER JOIN City AS Ci ON  Cz.CityId = Ci.Id
 	WHERE Ci.Name = CityName AND Cz.Age > PersonAge;
-END
+END;
+
+CREATE PROCEDURE GetCitizensWithCities()
+BEGIN
+    SELECT FirstName, LastName, Name AS CityName FROM Citizen AS Cz
+	INNER JOIN City AS Ci ON  Cz.CityId = Ci.Id;
+END;
+
+CREATE PROCEDURE GetCitizensWithCitiesByCity(IN CityName NVARCHAR(200))
+BEGIN
+    SELECT Cz.FirstName, Cz.LastName, Ci.Name AS CityName FROM Citizen AS Cz
+	INNER JOIN City AS Ci ON Cz.CityId = Ci.Id
+	WHERE Ci.Name = CityName;
+END;
