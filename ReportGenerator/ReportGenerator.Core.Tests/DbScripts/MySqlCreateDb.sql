3efb6f92d5f604ebd742184973ba875e4abe8c38
Ushakov Michael <um.nix.user@gmail.com>
2019-01-13 16:47:32 +0300

    new config for testing report generation with stored procedure with mysql

--- ReportGenerator/ReportGenerator.Core.Tests/DbScripts/MySqlCreateDb.sql
+++ ReportGenerator/ReportGenerator.Core.Tests/DbScripts/MySqlCreateDb.sql
@@ -27,3 +27,23 @@ CREATE VIEW CitizensWithRegion AS
 SELECT Cz.FirstName, Cz.LastName, Cz.Age, Cz.Sex, Ci.Name AS City, R.Name AS Region 
 FROM Citizen AS Cz INNER JOIN City AS Ci ON Cz.CityId = Ci.Id
 INNER JOIN Region AS R ON Ci.RegionId = R.Id;
+
+CREATE PROCEDURE [dbo].SelectCitizensWithCities()
+BEGIN
+    SELECT [FirstName], [LastName], [Name] AS CityName FROM [dbo].[Citizen] AS Cz
+	INNER JOIN [dbo].[City] AS Ci ON  Cz.CityId = Ci.Id;
+END
+
+CREATE PROCEDURE [dbo].SelectCitizensWithCitiesByCity(IN City NVARCHAR(200))
+BEGIN
+    SELECT Cz.[FirstName], Cz.[LastName], Ci.[Name] AS CityName FROM [dbo].[Citizen] AS Cz
+	INNER JOIN [dbo].[City] AS Ci ON Cz.CityId = Ci.Id
+	WHERE Ci.[Name] = City;
+END
+
+CREATE PROCEDURE [dbo].SelectCitizensWithCitiesByCityAndAge(IN City NVARCHAR(200), IN PersonAge INT)
+BEGIN
+    SELECT Cz.[FirstName], Cz.[LastName], Cz.[Age], Ci.[Name] AS CityName FROM [dbo].[Citizen] AS Cz
+	INNER JOIN [dbo].[City] AS Ci ON  Cz.CityId = Ci.Id
+	WHERE Ci.[Name] = City AND Cz.[Age] > PersonAge;
+END'
