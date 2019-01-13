42ffc20fc2113c338e3eb4c1b848e7db7c794d87
Ushakov Michael <um.nix.user@gmail.com>
2019-01-13 20:21:14 +0300

    finally MySql works with stored procedures also

--- ReportGenerator/ReportGeneratorCore/Database/Factories/DbParameterFactory.cs
+++ ReportGenerator/ReportGeneratorCore/Database/Factories/DbParameterFactory.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
 using MySql.Data.MySqlClient;
@@ -7,12 +8,19 @@ namespace ReportGenerator.Core.Database.Factories
 {
     public static class DbParameterFactory
     {
-        public static DbParameter Create(DbEngine dbEngine, string parameterName, int parameterType)
+        public static DbParameter Create(DbEngine dbEngine, string parameterName, int parameterType, 
+                                         ParameterDirection parameterDirection = ParameterDirection.Input)
         {
             if (dbEngine == DbEngine.SqlServer)
-                return new SqlParameter(parameterName, parameterType);
+                return new SqlParameter(parameterName, (SqlDbType)parameterType)
+                {
+                    Direction = parameterDirection
+                };
             if (dbEngine == DbEngine.MySql)
-                return new MySqlParameter(parameterName, parameterType);
+                return new MySqlParameter(parameterName, (MySqlDbType)parameterType)
+                {
+                    Direction = parameterDirection
+                };
             throw new NotImplementedException("Other db engine are not supported yet, please add a github issue https://github.com/EvilLord666/ReportGenerator");
         }
     }
