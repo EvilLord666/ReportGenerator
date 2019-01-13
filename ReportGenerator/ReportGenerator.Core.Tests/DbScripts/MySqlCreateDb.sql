57fc5e2658b0141494e6ec470fc97a6f5e66a3b7
Ushakov Michael <um.nix.user@gmail.com>
2019-01-13 18:33:18 +0300

    solved issue with dropping mysql DB

--- ReportGenerator/ReportGenerator.Core.Tests/DbScripts/MySqlCreateDb.sql
+++ ReportGenerator/ReportGenerator.Core.Tests/DbScripts/MySqlCreateDb.sql
@@ -28,22 +28,9 @@ SELECT Cz.FirstName, Cz.LastName, Cz.Age, Cz.Sex, Ci.Name AS City, R.Name AS Reg
 FROM Citizen AS Cz INNER JOIN City AS Ci ON Cz.CityId = Ci.Id
 INNER JOIN Region AS R ON Ci.RegionId = R.Id;
 
-CREATE PROCEDURE [dbo].SelectCitizensWithCities()
+CREATE PROCEDURE GetCitizensByCityAndAge(IN CityName NVARCHAR(200), IN PersonAge INTEGER)
 BEGIN
-    SELECT [FirstName], [LastName], [Name] AS CityName FROM [dbo].[Citizen] AS Cz
-	INNER JOIN [dbo].[City] AS Ci ON  Cz.CityId = Ci.Id;
+    SELECT Cz.FirstName, Cz.LastName, Cz.Age, Ci.Name AS CityName FROM Citizen AS Cz
+	INNER JOIN City AS Ci ON  Cz.CityId = Ci.Id
+	WHERE Ci.Name = CityName AND Cz.Age > PersonAge;
 END
-
-CREATE PROCEDURE [dbo].SelectCitizensWithCitiesByCity(IN City NVARCHAR(200))
-BEGIN
-    SELECT Cz.[FirstName], Cz.[LastName], Ci.[Name] AS CityName FROM [dbo].[Citizen] AS Cz
-	INNER JOIN [dbo].[City] AS Ci ON Cz.CityId = Ci.Id
-	WHERE Ci.[Name] = City;
-END
-
-CREATE PROCEDURE [dbo].SelectCitizensWithCitiesByCityAndAge(IN City NVARCHAR(200), IN PersonAge INT)
-BEGIN
-    SELECT Cz.[FirstName], Cz.[LastName], Cz.[Age], Ci.[Name] AS CityName FROM [dbo].[Citizen] AS Cz
-	INNER JOIN [dbo].[City] AS Ci ON  Cz.CityId = Ci.Id
-	WHERE Ci.[Name] = City AND Cz.[Age] > PersonAge;
-END'
