8cfb28a8bb636ca5ab69b678a969a599af54edb1
Ushakov Michael <um.nix.user@gmail.com>
2019-01-15 21:58:59 +0300

    intermediate: test is ready, but does not work

--- ReportGenerator/ReportGenerator.Core.Tests/ReportsGenerator/TestExcelReportGeneratorManager.cs
+++ ReportGenerator/ReportGenerator.Core.Tests/ReportsGenerator/TestExcelReportGeneratorManager.cs
@@ -49,7 +49,14 @@ namespace ReportGenerator.Core.Tests.ReportsGenerator
         public void TestGenerateReportPostgres()
         {
             SetUpPostgresSqlTestData();
-            // todo: test with postgres ...
+            object[] parameters = ExcelReportGeneratorHelper.CreateParameters(1, 2, 3);
+            ILoggerFactory loggerFactory = new LoggerFactory();
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+            IReportGeneratorManager manager = new ExcelReportGeneratorManager(loggerFactory, DbEngine.PostgresSql, _connectionString);
+            Task<bool> result = manager.GenerateAsync(TestExcelTemplate, PostgresSqlDataExecutionConfig, ReportFile, parameters);
+            result.Wait();
+            Assert.True(result.Result);
             TearDownPostgresSqlTestData();
         }
         
@@ -139,7 +146,21 @@ namespace ReportGenerator.Core.Tests.ReportsGenerator
 
         private void SetUpPostgresSqlTestData()
         {
-            
+            _dbManager = new CommonDbManager(DbEngine.PostgresSql, _loggerFactory.CreateLogger<CommonDbManager>());
+            IDictionary<string, string> connectionStringParams = new Dictionary<string, string>()
+            {
+                {DbParametersKeys.HostKey, TestPostgresSqlHost},
+                {DbParametersKeys.DatabaseKey, TestPostgresSqlDatabase},
+                // {DbParametersKeys.UseIntegratedSecurityKey, "true"} // is not working ...
+                {DbParametersKeys.LoginKey, "postgres"},
+                {DbParametersKeys.PasswordKey, "123"}
+            };
+            _connectionString = ConnectionStringBuilder.Build(DbEngine.PostgresSql, connectionStringParams);
+            _dbManager.CreateDatabase(_connectionString, true);
+            string createDatabaseStatement = File.ReadAllText(Path.GetFullPath(PostgresSqlCreateDatabaseScript));
+            string insertDataStatement = File.ReadAllText(Path.GetFullPath(PostgresSqlInsertDataScript));
+            _dbManager.ExecuteNonQueryAsync(_connectionString, createDatabaseStatement).Wait();
+            _dbManager.ExecuteNonQueryAsync(_connectionString, insertDataStatement).Wait();
         }
         
         private void TearDownPostgresSqlTestData()
@@ -152,12 +173,15 @@ namespace ReportGenerator.Core.Tests.ReportsGenerator
         private const string SqlServerDataExecutionConfig = @"..\..\..\ExampleConfig\sqlServerDataExtractionParams.xml";
         private const string SqLiteDataExecutionConfig = @"..\..\..\ExampleConfig\sqLiteDataExtractionParams.xml";
         private const string MySqlDataExecutionConfig = @"..\..\..\ExampleConfig\mySql_testReport4_StoredProcedure.xml";
+        private const string PostgresSqlDataExecutionConfig = @"..\..\..\postgresViewDataExtractionParams.xml";
 
         private const string TestSqlServerHost = @"(localdb)\mssqllocaldb";
         private const string TestSqlServerDatabasePattern = "ReportGeneratorTestDb";
         private const string TestSqLiteDatabase = "ReportGeneratorTestDb.sqlite";
         private const string TestMySqlHost = "localhost";
         private const string TestMySqlDatabase = "ReportGeneratorTestDb";
+        private const string TestPostgresSqlHost = "localhost";
+        private const string TestPostgresSqlDatabase = "ReportGeneratorTestDb";
 
         private const string SqlServerCreateDatabaseScript = @"..\..\..\DbScripts\SqlServerCreateDb.sql";
         private const string SqlServerInsertDataScript = @"..\..\..\DbScripts\SqlServerCreateData.sql";
@@ -165,6 +189,8 @@ namespace ReportGenerator.Core.Tests.ReportsGenerator
         private const string SqLiteInsertDataScript = @"..\..\..\DbScripts\SqLiteCreateData.sql";
         private const string MySqlCreateDatabaseScript = @"..\..\..\DbScripts\MySqlCreateDb.sql";
         private const string MySqlInsertDataScript = @"..\..\..\DbScripts\MySqlCreateData.sql";
+        private const string PostgresSqlCreateDatabaseScript = @"..\..\..\DbScripts\PostgresSqlCreateDb.sql";
+        private const string PostgresSqlInsertDataScript = @"..\..\..\DbScripts\PostgresSqlCreateData.sql";
 
         private string _testSqlServerDbName;
         private string _connectionString;
