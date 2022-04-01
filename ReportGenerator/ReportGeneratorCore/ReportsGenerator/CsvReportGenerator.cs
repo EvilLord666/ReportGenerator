42a8196d491d09692f8f8b093dc1b84c15e4a8a7
Ushakov Michael <um.nix.user@gmail.com>
2022-04-01 11:20:54 +0500

    created Csv Generator

--- ReportGenerator/ReportGeneratorCore/ReportsGenerator/CsvReportGenerator.cs
+++ ReportGenerator/ReportGeneratorCore/ReportsGenerator/CsvReportGenerator.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using ReportGenerator.Core.Data;
@@ -14,10 +19,61 @@ namespace ReportGenerator.Core.ReportsGenerator
             _reportFile = reportFile;
         }
 
-        public Task<bool> GenerateAsync(DbData data, object[] parameters)
+        public async Task<bool> GenerateAsync(DbData data, object[] parameters)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                // 1. Drop reportFile if exists & create
+                if (File.Exists(_reportFile))
+                    File.Delete(_reportFile);
+                // we assume that valid file was passed here (at least have .csv extension)
+                File.Create(_reportFile);
+                // 2. Write CSV header to reportFile
+                string[] headers = File.ReadAllLines(_template);
+                File.WriteAllLines(_reportFile, headers);
+                // 3. Get Batch from dbData
+                int batchCounter = 0;
+                while (true)
+                {
+                    IList<IList<DbValue>> rowsBatch = data.Rows.Skip(batchCounter * BatchSize).Take(BatchSize).ToList();
+                    batchCounter++;
+                    // 4. Append every batch to the end of file
+                    if (rowsBatch.Any())
+                    {
+                        IList<string> linesToAdd = rowsBatch.Select(r => CreateCsvRow(r)).ToList();
+                        File.AppendAllLines(_reportFile, linesToAdd);
+                    }
+                    // 5. Stop if number of rows < BatchSize
+                    if (rowsBatch.Count < BatchSize)
+                    {
+                        break;
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"An error occurred during csv report generation: {e.Message}");
+                return false;
+            }
         }
+
+        private string CreateCsvRow(IList<DbValue> columns)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (DbValue column in columns)
+            {
+                if (builder.Length > 0)
+                    builder.Append(_separator);
+                builder.Append(column.Value);
+            }
+
+            builder.Append(Environment.NewLine);
+            return builder.ToString();
+        }
+
+        private const int BatchSize = 50000;
 
         private readonly ILogger<CsvReportGenerator> _logger;
         private readonly string _template;
